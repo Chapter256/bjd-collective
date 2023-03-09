a9a2b23dcf8645a5b8330a2d21212c584c81b4d1
Camilla Sonesson <Camilla_Sonesson@hotmail.com>
2023-03-09 04:24:15 +0000

    removed double code

--- bjdcollective/settings.py
+++ bjdcollective/settings.py
@@ -115,6 +115,8 @@ WSGI_APPLICATION = 'bjdcollective.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -158,9 +160,6 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
