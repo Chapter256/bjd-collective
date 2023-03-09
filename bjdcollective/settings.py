1933769e39fb3aa714fee5b29408878643d22ad5
Camilla Sonesson <Camilla_Sonesson@hotmail.com>
2023-03-09 14:26:00 +0000

    removed decoding

--- bjdcollective/settings.py
+++ bjdcollective/settings.py
@@ -112,7 +112,7 @@ WSGI_APPLICATION = 'bjdcollective.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL").decode())
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
